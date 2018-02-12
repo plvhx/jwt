e1f40d1807f8ca6d9be2031f6c2e934d3ef3787f
plvhx <gandung@slackerc0de.us>
2018-02-12 10:58:19 +0700

    Adding tests for accessor subsystem..

---
+++ tests/Accessor/JoseHeaderAccessorTest.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Gandung\JWT\Tests\Accessor;
+
+use PHPUnit\Framework\TestCase;
+use Gandung\JWT\Accessor\JoseHeaderAccessor;
+use Gandung\JWT\JWTFactory;
+use Gandung\JWT\Token\JoseBuilderInterface;
+
+/**
+ * @author Paulus Gandung Prakosa <rvn.plvhx@gmail.com>
+ */
+class JoseHeaderAccessorTest extends TestCase
+{
+	/**
+	 * @var JoseBuilderInterface
+	 */
+	private $jose;
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->jose = JWTFactory::getJoseBuilder()
+			->algorithm(\Gandung\JWT\Token\Algorithm::HS256)
+			->type('JWT')
+			->contentType('application/json');
+	}
+
+	public function testCanGetInstance()
+	{
+		$this->assertInstanceOf(JoseHeaderAccessor::class, new JoseHeaderAccessor($this->jose->getValue()));
+	}
+
+	public function testCanGetAlgorithmDirective()
+	{
+		$accessor = new JoseHeaderAccessor($this->jose->getValue());
+		$this->assertInternalType('string', $accessor->getAlgorithm());
+		$this->assertEquals(\Gandung\JWT\Token\Algorithm::HS256, $accessor->getAlgorithm());
+	}
+
+	/**
+	 * @expectedException \RuntimeException
+	 */
+	public function testCanGetJwkSetUrlDirective()
+	{
+		$accessor = new JoseHeaderAccessor($this->jose->getValue());
+		$this->assertInternalType('string', $accessor->getJwkSetUrl());
+	}
+
+	/**
+	 * @expectedException \RuntimeException
+	 */
+	public function testCanGetKeyID()
+	{
+		$accessor = new JoseHeaderAccessor($this->jose->getValue());
+		$this->assertInternalType('string', $accessor->getKeyID());
+	}
+
+	/**
+	 * @expectedException \RuntimeException
+	 */
+	public function testCanGetX509Url()
+	{
+		$accessor = new JoseHeaderAccessor($this->jose->getValue());
+		$this->assertInternalType('string', $accessor->getX509Url());
+	}
+
+	public function testCanGetType()
+	{
+		$accessor = new JoseHeaderAccessor($this->jose->getValue());
+		$this->assertInternalType('string', $accessor->getType());
+		$this->assertEquals('JWT', $accessor->getType());
+	}
+
+	public function testCanGetContentType()
+	{
+		$accessor = new JoseHeaderAccessor($this->jose->getValue());
+		$this->assertInternalType('string', $accessor->getContentType());
+		$this->assertEquals('application/json', $accessor->getContentType());
+	}
+
+	public function testCanGetAll()
+	{
+		$accessor = new JoseHeaderAccessor($this->jose->getValue());
+		$this->assertInternalType('array', $accessor->get());
+		$this->assertNotEmpty($accessor->getContentType());
+	}
+}
---
+++ tests/Accessor/PayloadAccessorTest.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Gandung\JWT\Tests\Accessor;
+
+use PHPUnit\Framework\TestCase;
+use Gandung\JWT\Accessor\PayloadAccessor;
+use Gandung\JWT\Token\PayloadBuilderInterface;
+use Gandung\JWT\JWTFactory;
+
+/**
+ * @author Paulus Gandung Prakosa <rvn.plvhx@gmail.com>
+ */
+class PayloadAccessorTest extends TestCase
+{
+	/**
+	 * @var
+	 */
+	private $payload;
+
+	public function __construct()
+	{
+		parent::__construct();
+		$claim = JWTFactory::getClaimBuilder()
+			->issuedBy('me')
+			->expireAt(new \DateTimeImmutable('@' . (time() + 3600)));
+		$this->payload = JWTFactory::getPayloadBuilder()
+			->claim($claim)
+			->userData([
+				'credentials' => [
+					'username' => 'me',
+					'password' => 'this_is_only_me_will_know'
+				]
+			]);
+	}
+
+	public function testCanGetInstance()
+	{
+		$this->assertInstanceOf(PayloadAccessor::class, new PayloadAccessor($this->payload->getValue()));
+	}
+
+	public function testCanGetIssuedByDirective()
+	{
+		$accessor = new PayloadAccessor($this->payload->getValue());
+		$this->assertInternalType('string', $accessor->getIssuedBy());
+		$this->assertEquals('me', $accessor->getIssuedBy());
+	}
+
+	/**
+	 * @expectedException \RuntimeException
+	 */
+	public function testCanGetRelatedToDirective()
+	{
+		$accessor = new PayloadAccessor($this->payload->getValue());
+		$this->assertInternalType('string', $accessor->getRelatedTo());
+	}
+
+	/**
+	 * @expectedException \RuntimeException
+	 */
+	public function testCanGetIntendedForDirective()
+	{
+		$accessor = new PayloadAccessor($this->payload->getValue());
+		$this->assertInternalType('string', $accessor->getIntendedFor());
+	}
+
+	public function testCanGetExpireAtDirective()
+	{
+		$accessor = new PayloadAccessor($this->payload->getValue());
+		$this->assertInstanceOf(\DateTimeImmutable::class, $accessor->getExpireAt());
+	}
+
+	/**
+	 * @expectedException \RuntimeException
+	 */
+	public function testCanGetCanOnlyBeUsedAfterDirective()
+	{
+		$accessor = new PayloadAccessor($this->payload->getValue());
+		$this->assertInstanceOf(\DateTimeImmutable::class, $accessor->getCanOnlyBeUsedAfter());
+	}
+
+	/**
+	 * @expectedException \RuntimeException
+	 */
+	public function testCanGetIssuedAtDirective()
+	{
+		$accessor = new PayloadAccessor($this->payload->getValue());
+		$this->assertInstanceOf(\DateTimeImmutable::class, $accessor->getIssuedAt());
+	}
+
+	/**
+	 * @expectedException \RuntimeException
+	 */
+	public function testCanGetIdentifiedByDirective()
+	{
+		$accessor = new PayloadAccessor($this->payload->getValue());
+		$this->assertInstanceOf(\DateTimeImmutable::class, $accessor->getIdentifiedBy());
+	}
+
+	public function testCanGetAll()
+	{
+		$accessor = new PayloadAccessor($this->payload->getValue());
+		$this->assertInternalType('array', $accessor->get());
+		$this->assertNotEmpty($accessor->get());
+	}
+}
